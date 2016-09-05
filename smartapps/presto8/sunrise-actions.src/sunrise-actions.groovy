8f42f31f8901a843aee175efd269f054c905232a
Preston Hunt <me@prestonhunt.com>
2016-09-05 09:31:21 -0700

    add sunrise

---
+++ smartapps/presto8/sunrise-actions.src/sunrise-actions.groovy
@@ -0,0 +1,47 @@
+/**
+ *  Copyright 2016 Preston Hunt
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ *  Author: SmartThings
+ *  Author: Preston Hunt
+ */
+definition(
+    name: "Sunrise Actions",
+    namespace: "presto8",
+    author: "me@prestonhunt.com",
+    description: "Take various actions at sunrise each day.",
+    category: "Convenience",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png"
+)
+
+preferences {
+    input "switches", "capability.switch", title: "Which lights to brighten to 100%?", multiple: true
+}
+
+def installed() {
+}
+
+def updated() {
+    // called whenever the user changes any preferences, we also use this do to init instead of installed()
+
+    unsubscribe()
+    subscribe(location, "sunrise", sunriseHandler)
+    
+    log.trace "installed and monitoring"
+}
+
+def sunsetHandler(evt) {
+    log.trace "sunriseHandler()"
+
+    log.trace "brightening lights to 100%"
+    switches.setLevel(100)
+}
